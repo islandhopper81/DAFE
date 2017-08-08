59943d4cece055edbc40c6c815216186ad56d054
Scott Yourstone <scott.yourstone81@gmail.com>
2017-08-08 11:16:57 -0400

    adding DAFE_vis_genome_source_vs_DA.R  a script to compare the genome source to how many RZ and BK enrichments that genome contains

---
+++ scripts/DAFE_vis_genome_source_vs_DA.R
@@ -0,0 +1,68 @@
+#!/usr/bin/evn Rscript
+
+### Description
+# create a figure showing how the DA values match against the source 
+# of the genome.
+
+### R Libraries
+require("getopt", quietly=T)
+require("ggplot2", quietly=T)
+require("reshape2", quietly=T)
+
+### Defualt variables
+verbose = FALSE
+
+### Parameter variables
+# The params matrix
+# Each row is a parameter with 4 columns: 
+#   1) long value, 
+#   2) short value, 
+#   3) argument type where 0 = no argument, 1 = required , 2 = optional
+#   4) data type (logical, integer, double, complex, character)
+# When you call this script the args are named like --verbose or -v
+params = matrix(c(
+  "da_tbl", "d", 1, "character",
+  "genome_meta", "g", 1, "character",
+  "out", "o", 1, "character",
+  "verbose", "v", 0, "logical"
+  ), byrow=TRUE, ncol=4)
+opt = getopt(params)
+
+# define parameter specified varaibles
+if (! is.null(opt$verbose)) {
+  verbose = opt$verbose
+}
+
+
+### Functions
+main = function() {
+	# read in the genome metadata
+	meta = read.table(opt$genome_meta, header=T, row.names=1, sep="\t", quote="", comment.char="")
+
+	# read in the da table
+	da = read.table(opt$da_tbl, header=T, row.names=1, sep="\t")
+
+	# count the number of RZ enriched and BK enriched instances for each genome
+	rz_up = apply(da, 2, function(x) sum(x==1))
+	bk_up = apply(da, 2, function(x) sum(x==-1))
+
+	names = colnames(da)
+	names = sapply(names, function(x) substring(x, 2))
+	tmp_df = data.frame("genome" = names, "rz_up" = rz_up, "bk_up" = bk_up)
+
+	print(head(tmp_df))
+
+	tmp_df = melt(tmp_df)
+
+	# merge
+	final_df = merge(meta, tmp_df, by.x="row.names", by.y="genome")
+
+	# make the figure
+	ggplot(final_df, aes(x=Source, y=value, col=variable)) +
+		geom_boxplot(outlier.size=0) + 
+		geom_point()
+	ggsave(opt$out)
+}
+
+# run the main function to execute the program
+main()
