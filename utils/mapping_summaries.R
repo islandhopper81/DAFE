dce5d3a24198f04b691da1852e9c09ec3274a4f4
Scott Yourstone <scott.yourstone81@gmail.com>
2017-10-12 08:59:39 -0400

    now also makes a figure showing the number of reads that map according to the sample type they come from.  ie do more rhizosphere reads map than bulk soil reads

--- utils/mapping_summaries.R
+++ utils/mapping_summaries.R
@@ -22,6 +22,7 @@ verbose = FALSE
 params = matrix(c(
   "count_file", "c", 1, "character",
   "ref_meta_file", "r", 1, "character",
+  "sample_meta_file", "m", 1, "character",
   "out_prefix", "o", 1, "character",
   "title", "t", 1, "character",
   "verbose", "v", 0, "logical"
@@ -63,6 +64,7 @@ main = function() {
 	out_file = paste(opt$out_prefix, "_pa_npa_soil.pdf", sep="")
 	ggsave(out_file, p1) 
 
+	# make the plot graphed by genome environment
 	p2 = ggplot(meta, aes(x=Source, y=log10(counts))) +
 		geom_boxplot(outlier.size=NA) +
 		geom_point(position=position_jitter(width=.2)) +
@@ -76,7 +78,24 @@ main = function() {
 	out_file = paste(opt$out_prefix, "_iso_env.pdf", sep="")
 	ggsave(out_file, p2)
 
-		
+	# make the plot showing the number of reads that map from each 
+	# metagenome environment
+	sample_sums = apply(counts, 2, sum)
+	
+	sample_meta = read.table(opt$sample_meta_file, header=T, row.names=1, sep="\t")
+	sample_meta = sample_meta[names(sample_sums),]
+	sample_meta[,"counts"] = sample_sums
+
+	p3 = ggplot(sample_meta, aes(x=fraction, y=counts)) +
+		geom_boxplot(outlier.size=NA) +
+		geom_point(position=position_jitter(width=.2)) +
+		theme(text = element_text(size=20),
+			plot.title = element_text(hjust=0.5)) +
+		ylab("Reads Mapped (log10)") +
+		xlab("Metagenome Samples")
+
+	out_file = "reads_mapped_per_meta_env.pdf"
+	ggsave(out_file, p3)
 }
 
 # run the main function to execute the program
